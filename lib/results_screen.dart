a81cdd57bd1bbc22e1b1c03801ff0ca51d4e386f
rashmin <rashminxperience@gmail.com>
2026-01-11 21:19:02 +0545

    Handled logic to calculate and display results

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:techquiz/data/questions.dart';
+import 'package:techquiz/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -14,11 +34,13 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
-            const SizedBox(height: 30,),
-            const Text('List of answers and questions...'),
-            const SizedBox(height: 30,),
-            TextButton(onPressed: () {}, child: Text('Restart Quiz!'))
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+            ),
+            const SizedBox(height: 30),
+            QuestionsSummary(summaryData: summaryData),
+            const SizedBox(height: 30),
+            TextButton(onPressed: () {}, child: Text('Restart Quiz!')),
           ],
         ),
       ),
